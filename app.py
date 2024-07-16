fb671cf2654a65d734010caca53c606add886f89
AfkarIsh <ishaqafkar123@gmail.com>
2024-07-16 10:54:49 +0000

    jobitem added

--- app.py
+++ app.py
@@ -2,12 +2,33 @@ from flask import Flask, render_template
 #  object of the class
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+}, {
+    'id': 1,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': ' $11,00,000'
+}]
+
 
 @app.route("/")
 def hello_ArIsh():
-  return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='ArIsh')
 
 
 print(__name__)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
